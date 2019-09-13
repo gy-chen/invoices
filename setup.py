650382aa4543e8971e2bbea16ec5c23dd66b6268
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-09-13 20:50:50 +0800

    init

---
+++ setup.py
@@ -0,0 +1,27 @@
+from io import open
+
+from setuptools import find_packages, setup
+
+with open('invoices/__init__.py', 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.strip().split('=')[1].strip(' \'"')
+            break
+    else:
+        version = '0.0.1'
+
+REQUIRES = []
+
+setup(
+    name='invoices',
+    version=version,
+    description='',
+    author='GYCHEN',
+    author_email='gy.chen@gms.nutc.edu.tw',
+    maintainer='GYCHEN',
+    maintainer_email='gy.chen@gms.nutc.edu.tw',
+    url='https://github.com/gy-chen/invoices',
+    install_requires=REQUIRES,
+    tests_require=['coverage', 'pytest'],
+    packages=find_packages()
+)
