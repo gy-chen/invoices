85ee97f84027f966521fd8df2039cd0e5d57e646
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-11-03 03:03:50 +0000

    add prize crawler

--- invoices/crawler/prize.py
+++ invoices/crawler/prize.py
@@ -35,14 +35,32 @@ class PrizeSpider(scrapy.Spider):
     name = "prizespider"
     start_urls = ["http://invoice.etax.nat.gov.tw/"]
 
-    _PAT_DATE = re.compile(r"\W(\d{3})年\d({2}-\d{2})月\W")
+    _PAT_DATE = re.compile(r"\W(\d{3})年(\d{2}-\d{2})月\W")
     _PAT_NORMAL_PRIZES = re.compile(r"\b\d{8}\b")
     _PAT_SIXTH_PRIZES = re.compile(r"\b\d{3}\b")
 
-    def parse(self, response):
-        year, month_range = response.css("#area1").re(self._PAT_DATE)
+    _RAW_MONTH_RANGE_MAPPING = {
+        "01-02": PrizeMonthEnum.MONTH_1_2,
+        "03-04": PrizeMonthEnum.MONTH_3_4,
+        "05-06": PrizeMonthEnum.MONTH_5_6,
+        "07-08": PrizeMonthEnum.MONTH_7_8,
+        "09-10": PrizeMonthEnum.MONTH_9_10,
+        "11-12": PrizeMonthEnum.MONTH_11_12,
+    }
 
-        normals = response.css("#area1 table").re(self._PAT_NORMAL_PRIZES)
+    def parse(self, response):
+        area1 = response.css("#area1")
+        yield from self._parse_area(area1)
+        area2 = response.css("#area2")
+        yield from self._parse_area(area2)
+
+    def _parse_area(self, selector):
+        year_raw, month_range_raw = selector.re(self._PAT_DATE)
+
+        year = self._parse_year(year_raw)
+        month_range = self._parse_month_range(month_range_raw)
+
+        normals = selector.re(self._PAT_NORMAL_PRIZES)
         if len(normals) != 5:
             raise ValueError("unexpect invoice page format")
         normal_types = [
@@ -53,5 +71,10 @@ class PrizeSpider(scrapy.Spider):
             PrizeTypeEnum.FIRST_AWARD,
         ]
         for number, type in zip(normals, normal_types):
-            yield PrizeItem()
+            yield PrizeItem(year=year, month=month_range, type=type, number=number)
 
+    def _parse_year(self, raw):
+        return int(raw)
+
+    def _parse_month_range(self, raw):
+        return self._RAW_MONTH_RANGE_MAPPING[raw]
