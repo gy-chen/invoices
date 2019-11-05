8792e3eaefee7d8918337234c1ecdc348359216e
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-11-05 07:41:44 +0000

    add save prize pipeline

--- crawler_setting.py
+++ crawler_setting.py
@@ -1,5 +1,7 @@
 PrizeCrawlerSetting = {
     'ITEM_PIPELINES': {
         'invoices.crawler.prize.SpawnSubPrizesPipeline': 100,
-    }
+        'invoices.crawler.model_pipeline.SavePrizePipeline': 200
+    },
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:///:memory:'
 }
--- run_prize_crawler.py
+++ run_prize_crawler.py
@@ -1,7 +1,18 @@
 from scrapy.crawler import CrawlerProcess
-from crawler_setting import PrizeCrawlerSetting
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from crawler_setting import PrizeCrawlerSetting as setting
+from invoices.crawler import context
 from invoices.crawler.prize import PrizeSpider
+from invoices.model import PrizeModel
 
-cp = CrawlerProcess(PrizeCrawlerSetting)
+engine = create_engine(setting['SQLALCHEMY_DATABASE_URI'])
+Session = sessionmaker(bind=engine)
+session = Session()
+context.prize_model = PrizeModel(session)
+
+cp = CrawlerProcess(setting)
 cp.crawl(PrizeSpider)
 cp.start()
+
+session.commit()
