d0d360ff6255bb77ea47c72b76d01ba0a49700f3
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-11-10 17:31:17 +0800

    add config sample

---
+++ config.sample.py
@@ -0,0 +1,16 @@
+SECRET_KEY = 'your flask secret key'
+
+OAUTH_CLIENT_ID = (
+    "your oauth client id"
+)
+
+OAUTH_CLIENT_SECRET = "your oauth client secret"
+OAUTH_AUTHORIZATION_BASE_URL = "https://accounts.google.com/o/oauth2/v2/auth"
+OAUTH_REDIRECT_URI = "http://127.0.0.1:5000/login_callback"
+OAUTH_TOKEN_URL = "https://oauth2.googleapis.com/token"
+OAUTH_SCOPE = "openid email"
+
+LOGIN_JWT_SECRET = "your login jwt secret"
+
+SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+
