5313d62b89e07cdeab08d6eb930973918aa0b736
GYCHEN <gy.chen@gms.nutc.edu.tw>
2019-11-13 09:26:04 +0000

    move more logic into job module

--- invoices/job/match_prizes.py
+++ invoices/job/match_prizes.py
@@ -1,3 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from invoices.model import PrizeModel, InvoiceMatchModel
 from invoices.model_prize_match import is_match, is_date_match
 
 
@@ -39,3 +42,15 @@ class PrizeMatcher:
             else:
                 if is_date_matched:
                     self._invoice_match_model.add_invoice_unmatch(invoice.id)
+
+
+def run_match_prizes_job(config):
+    engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    prize_model = PrizeModel(session)
+    invoice_match_model = InvoiceMatchModel(session)
+    prize_matcher = PrizeMatcher(prize_model, invoice_match_model)
+
+    prize_matcher.run()
+    session.commit()
